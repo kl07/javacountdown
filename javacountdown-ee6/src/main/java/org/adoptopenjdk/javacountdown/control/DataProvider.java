75c2b75500ad2580de3ce017f23b070ef9ff461c
Markus Eisele <myfear@web.de>
2013-03-13 06:24:06 +0100

    capitalization errror

--- javacountdown-ee6/src/main/java/org/adoptopenjdk/javacountdown/control/DataProvider.java
+++ javacountdown-ee6/src/main/java/org/adoptopenjdk/javacountdown/control/DataProvider.java
@@ -35,7 +35,7 @@ import javax.persistence.TypedQuery;
 @Named
 public class DataProvider {
 
-    private final static String GET_COUNTRIES = "SELECT new org.adoptopenjdk.javacountdown.control.CountryHolder(v.country, COUNT(v.country)) cnt FROM Visit v WHERE v.country <> 'unresolved' AND v.vminor = :version GROUP BY v.country ORDER BY COUNT(v.country)";
+    private final static String GET_COUNTRIES = "SELECT new org.adoptopenjdk.javacountdown.control.CountryHolder(v.country, COUNT(v.country)) cnt FROM Visit v WHERE v.country <> 'unresolved' AND v.vMinor = :version GROUP BY v.country ORDER BY COUNT(v.country)";
     private final static String GET_COUNTRY_FROM_GEO_DATA = "SELECT new org.adoptopenjdk.javacountdown.control.CountryHolder(G.alpha2) FROM Geonames AS G ORDER BY ABS((ABS(G.latitude-:lat))+(ABS(G.longitude-:lng))) ASC";
     private static final Logger logger = Logger.getLogger(DataProvider.class.getName());
     @PersistenceUnit(unitName = "javacountdownPU")
