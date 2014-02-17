2db82c356ad9731579886630589f201ae9da3ab2
Alex Theedom <alex.theedom@gmail.com>
2014-02-17 18:12:34 +0100

    Migration to MongoDB

---
+++ javacountdown-ee6/src/main/java/org/adoptopenjdk/javacountdown/control/DataAccessObject.java
@@ -0,0 +1,19 @@
+package org.adoptopenjdk.javacountdown.control;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import javax.inject.Qualifier;
+
+
+@Qualifier
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.FIELD, ElementType.METHOD})
+public @interface DataAccessObject {
+
+    Type value();
+    
+    enum Type{ VISIT, GEOPOSITION, REPORT } 
+
+}
